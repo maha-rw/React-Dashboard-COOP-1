64b62be7e2a61f646e8af15ab8eeda8403583d3f
maha-rw <maha.ibrahimrw@gmail.com>
2024-07-01 13:34:13 +0300

    Draft 1

---
+++ src/Data/Data.js
@@ -0,0 +1,54 @@
+// Sidebar imports
+import {
+  UilEstate,
+  UilClipboardAlt,
+  UilUsersAlt,
+  UilPackage,
+  UilChart,
+  UilSignOutAlt,
+} from "@iconscout/react-unicons";
+
+// Analytics Cards imports
+import { UilUsdSquare, UilMoneyWithdrawal } from "@iconscout/react-unicons";
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
+
+
+
+
+
+// Analytics Cards Data
+export const cardsData = [
+  {
+    title: "Learners Enrolled",
+    color: {
+      backGround: "linear-gradient(180deg, #bb67ff 0%, #c484f3 100%)",
+      boxShadow: "0px 10px 20px 0px #e0c6f5",
+    },
+    value: "3,156,821",
+    series: [
+      {
+        name: "Number of Enrolled",
+        data: [31, 40, 28, 51, 42, 109, 100],
+      },
+    ],
+  },
+  {
+    title: "Certificates Issued",
+    color: {
+      backGround: "linear-gradient(180deg, #FF919D 0%, #FC929D 100%)",
+      boxShadow: "0px 10px 20px 0px #FDC0C7",
+    },
+    value: "14,270",
+    series: [
+      {
+        name: "Revenue",
+        data: [10, 100, 50, 70, 80, 30, 40],
+      },
+    ],
+  },
+];
+
+
+
+
+
---
+++ src/components/Updates/Updates.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Updates.css";
+
+const Updates = () => {
+  return (
+    <div className="Updates">
+    </div>
+  );
+};
+
+export default Updates;
